84ac3465c3e67a9d255072d0eeb26bea8a202698
n30tc <neothecode@outlook.com>
2025-11-29 21:44:39 +0100

    renew branch file local copy manually

--- start.sh
+++ start.sh
@@ -1,23 +1,22 @@
-#!/bin/bash
+#!/bin/zsh
 
-echo "Création d'un nouveau projet React"
+echo "🎯 Création d'un nouveau projet React"
 echo ""
-echo "1 - ReactJS classique"
-echo "2 - ReactJS + TypeScript"
+echo "1️⃣  ReactJS classique"
+echo "2️⃣  ReactJS + TypeScript"
 echo ""
-
-read -p "Choisis une option (1 ou 2) : " choice
+read "choice?👉 Choisis une option (1 ou 2) : "
 
 if [[ "$choice" == "1" ]]; then
-  read -p "Nom du projet : " projectName
-  echo "Création du projet ReactJS simple : $projectName"
+  read "projectName?📦 Nom du projet : "
+  echo "🚀 Création du projet ReactJS simple : $projectName"
   npm create vite@latest "$projectName" -- --template react
 elif [[ "$choice" == "2" ]]; then
-  read -p "Nom du projet : " projectName
-  echo "Création du projet ReactJS + TypeScript : $projectName"
+  read "projectName?📦 Nom du projet : "
+  echo "🚀 Création du projet ReactJS + TypeScript : $projectName"
   npm create vite@latest "$projectName" -- --template react-ts
 else
   echo "❌ Option invalide. Relance le script et choisis 1 ou 2."
-fi
+fi 
 
-exit 0
+exit
